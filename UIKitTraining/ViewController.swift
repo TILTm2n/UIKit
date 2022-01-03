9497973958cb02e8c78c4e2af0622f759b06f072
Eugene <evgen29031999@bk.ru>
2022-01-03 23:19:13 +0300

    создаем UIActivityViewController

--- UIKitTraining/ViewController.swift
+++ UIKitTraining/ViewController.swift
@@ -10,40 +10,50 @@ import AVFoundation //фрэймворк для работы с аудио и в
 
 class ViewController: UIViewController {
     
-    var segmentControl = UISegmentedControl()
-    var imageView = UIImageView()
-    
-    var menuArray = ["one", "two", "three"]
-    var imageArray = [UIImage(named: "sunrise"),
-                      UIImage(named: "canada"),
-                      UIImage(named: "portret")]
+    var buttonShare = UIButton()
+    var textField = UITextField()
+    var activityViewController: UIActivityViewController? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createTextField()
+        createButton()
         
-        //create imageView
-        self.imageView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
-        self.imageView.center = self.view.center
-        self.imageView.image = self.imageArray[0]
-        self.view.addSubview(self.imageView)
-        
-        //create segment
-        self.segmentControl = UISegmentedControl(items: self.menuArray)
-        self.segmentControl.frame = CGRect(x: 95, y: 530, width: 200, height: 30)
-        self.view.addSubview(self.segmentControl)
-        
-        self.segmentControl.addTarget(self, action: #selector(changeSegment(target:)), for: .valueChanged)
     }
     
+    //MARK: - Method
+    func createTextField(){
+        self.textField.frame = CGRect(x: 0, y: 0, width: 280, height: 30)
+        self.textField.center = self.view.center
+        self.textField.borderStyle = UITextField.BorderStyle.roundedRect
+        self.textField.placeholder = "Enter text to share"
+        
+        self.view.addSubview(self.textField)
+    }
+    
+    func createButton(){
+        self.buttonShare = UIButton(type: .roundedRect)
+        self.buttonShare.frame = CGRect(x: 50, y: 350, width: 280, height: 44)
+        self.buttonShare.setTitle("Share", for: .normal)
+        self.buttonShare.addTarget(self, action: #selector(handleShare(paramSender:)), for: .touchUpInside)
+        self.view.addSubview(self.buttonShare)
+    }
     
     @objc
-    func changeSegment(target: UISegmentedControl){
-        if target == segmentControl{
-            let segmentIndex = target.selectedSegmentIndex
-            self.imageView.image = self.imageArray[segmentIndex]
-            let pr = target.titleForSegment(at: segmentIndex)
-            print(pr ?? "")
+    func handleShare(paramSender: Any){
+        let text = self.textField.text
+        if ((text?.isEmpty) != nil){
+            let message = "Сначала введите текст"
+            let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(action)
+            
+            self.present(alertController, animated: true, completion: nil)
         }
+        
+        self.activityViewController = UIActivityViewController(activityItems: [self.textField.text ?? "nil"], applicationActivities: nil)
+        self.present(self.activityViewController!, animated: true, completion: nil)
     }
 
 }
