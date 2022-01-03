15af33245f1eae7075ceeb52dca43c78cf2ca833
Eugene <evgen29031999@bk.ru>
2022-01-03 16:34:19 +0300

    создание UIPickerView

--- UIKitTraining/ViewController.swift
+++ UIKitTraining/ViewController.swift
@@ -8,66 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //Создали switch
-    let mySwitch = UISwitch()
-    let button = UIButton()
+    
+    let picker = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//                                      //var1
-//        //определили его положение и размеры
-//        self.mySwitch.frame = CGRect(x: 100, y: 100, width: 0, height: 0)
-//
-//        //разместили его в главном self.view
-//        self.view.addSubview(self.mySwitch)
-//
-//        //вкл/выкл по дефолту
-//        self.mySwitch.setOn(true, animated: true)
-//
-//        self.mySwitch.addTarget(self, action: #selector(switchChange(paramTarget:)), for: .valueChanged)
+        picker.center = self.view.center
         
-                                        //var2
-        self.mySwitch.frame = CGRect.zero
-        self.button.frame = CGRect.init(x: 100, y: 200, width: 200, height: 100)
+        picker.dataSource = self
+        picker.delegate = self
         
-        self.mySwitch.center = self.view.center
-        self.mySwitch.tintColor = UIColor.green
-        self.mySwitch.thumbTintColor = UIColor.red
-        self.mySwitch.onTintColor = UIColor.blue
-        
-        self.button.backgroundColor = UIColor.orange
-        self.button.setTitle("OK", for: .normal)
-        self.button.setTitle("pressed", for: .highlighted)
-
-        self.view.addSubview(self.mySwitch)
-        self.view.addSubview(self.button)
-        
-        self.mySwitch.addTarget(self, action: #selector(blocking(target:)), for: .valueChanged)
+        self.view.addSubview(picker)
         
     }
+
+}
+
+extension ViewController: UIPickerViewDataSource{
     
-//                                      //var 1
-//    @objc
-//    func switchChange(paramTarget: UISwitch){
-//        print("param is = \(paramTarget)")
-//        if paramTarget.isOn {
-//            print("is on")
-//        }else{
-//            print("is off")
-//        }
-//    }
-    
-                                        //var2
-    @objc
-    func blocking(target: UISwitch){
-        if target.isOn{
-            self.button.isUserInteractionEnabled = true
-        }else{
-            self.button.isUserInteractionEnabled = false
-        }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 100
+    }
+}
 
-
+extension ViewController: UIPickerViewDelegate{
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let result = "row \(row)"
+        
+        return result
+    }
 }
 
