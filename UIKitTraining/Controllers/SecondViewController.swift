111c18a84a9e217a76026580ce1ce637462b3c9f
Eugene <evgen29031999@bk.ru>
2022-01-05 18:35:01 +0300

    создаем UITextField

--- UIKitTraining/Controllers/SecondViewController.swift
+++ UIKitTraining/Controllers/SecondViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    var myTextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,8 +20,13 @@ class SecondViewController: UIViewController {
         
         var tabBarItem = UITabBarItem()
         tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
-        
         self.tabBarItem = tabBarItem
+        
+        createTextField()
+        myTextField.delegate = self
+        
+        //центр событий и обработчики событий
+        NotificationCenter.default.addObserver(self, selector: #selector(textFieldTextDidChange(ncParam:)), name: UITextField.textDidChangeNotification, object: nil)
         
     }
     
@@ -45,6 +52,59 @@ class SecondViewController: UIViewController {
         
     }
 
+    // MARK: - CreateUI
+    fileprivate func createTextField() {
+        let textFieldFrame = CGRect(x: 0, y: 0, width: 200, height: 31)
+        myTextField = UITextField(frame: textFieldFrame)
+        myTextField.borderStyle = .roundedRect
+        myTextField.contentVerticalAlignment = .center
+        myTextField.contentHorizontalAlignment = .center
+        myTextField.placeholder = "I swift deweloper"
+        myTextField.center = self.view.center
+        self.view.addSubview(myTextField)
+    }
+    
+    
+    // MARK: - UITextFieldDelegate
+//    //при выделении поля определяет редактируемо оно или нет
+//    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+//        print("это поле можно редактировать")
+//        return true
+//    }
+//    //срабатывает когда начался ввод
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        print("редактирование началось")
+//    }
+//
+//    //срабатывает когда покидаем поле
+//    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+//        print("редактирование закончилось")
+//        return true
+//    }
+//
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        print(string)
+//        return true
+//    }
+//
+//    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+//        print("очищено")
+//        return true
+//    }
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        print("хотите убрать клавиатуру")
+//        if textField == myTextField{
+//            self.myTextField.resignFirstResponder()
+//        }
+//        return true
+//    }
+//
+    // MARK: - Notification
+    @objc func textFieldTextDidChange(ncParam: NSNotification){
+        print(ncParam)
+    }
+    
     /*
     // MARK: - Navigation
 
