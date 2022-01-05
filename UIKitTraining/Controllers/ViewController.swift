b904477307494ec7669f549ffd21592e2caad902
Eugene <evgen29031999@bk.ru>
2022-01-05 10:34:23 +0300

    создаем UILabel

--- UIKitTraining/Controllers/ViewController.swift
+++ UIKitTraining/Controllers/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +22,20 @@ class ViewController: UIViewController {
         tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 0)
         
         self.tabBarItem = tabBarItem
+        
+        let labelFrame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        
+        label.frame = labelFrame
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.text = "IOS 15 and Xcode 13"
+        label.adjustsFontSizeToFitWidth = true
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.center = self.view.center
+        label.sizeToFit()
+        label.shadowColor = UIColor.lightGray
+        label.shadowOffset = CGSize(width: 2, height: 2)
+        view.addSubview(label)
         
     }
     
